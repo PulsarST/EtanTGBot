cd80e5ae5baf5374c05242c514ce817ee15e0cd4
PulsarST <tamerlan.zh25@gmail.com>
2026-02-11 00:05:10 +0500

    added schemas of student review, professor and subject

--- ai/text_processing.py
+++ ai/text_processing.py
@@ -1,7 +1,17 @@
 import json
 
+import base_settings
 
-async def process_raw_text(text: str) -> str: ...
+
+async def process_raw_text(text: str) -> str | None:
+    respone = base_settings.client.models.generate_content(
+        model=base_settings.model, contents=[base_settings.process_raw_text_promt, text]
+    )
+    return respone.text
 
 
-async def generate_json_from_text(text: str) -> dict[str, int | str]: ...
+async def generate_json_from_text(text: str) -> dict[str, int | str]:
+    response = base_settings.client.models.generate_content(
+        model=base_settings.model, contents=[]
+    )
+    return json.loads(response.text or "")
---
+++ database/schemes.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from typing import List
+
+from pydantic import BaseModel, Field
+
+
+class StudentsReview(BaseModel):
+    """Student Review is a schema for students reviews in DB."""
+
+    mark: int
+    """mark(int): defeined in [1, 10] and describes a studenet opinion in int about professor, or activity in univeristy."""
+
+    causes: List[str]
+    """causes(List[str]): a list of causes why student sends good, bad review about professor, or activity in university."""
+
+    professors_name: str
+    """professors_name(str): name of professor."""
+
+    subject: str
+    """subject(str): a name of subject."""
+
+    date: datetime = Field(default_factory=datetime.now)
+    """date(datetime): the date when review was sended."""
+
+
+class ProfessorSchema(BaseModel):
+    """The schema for describing a professors in DB"""
+
+    name: str
+    """name(str): the name of professr"""
+
+
+class SubjectSchema(BaseModel):
+    """The schema for describing a subjects in DB"""
+
+    name: str
+    """name(str): the name of subject"""
