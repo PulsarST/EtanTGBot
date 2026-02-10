d1a678dadb1b1ebb0d313d2b5139a91540074860
PulsarST <tamerlan.zh25@gmail.com>
2026-02-11 01:05:52 +0500

    Now we can get data from students !

--- ai/text_processing.py
+++ ai/text_processing.py
@@ -1,17 +1,44 @@
-import json
-
 import base_settings
+
+from database import schemes
 
 
 async def process_raw_text(text: str) -> str | None:
+    """
+    process a raw text from user:
+    1. corrects spelling mistakes
+    2. corrects punctuation mistakes
+    3. replaces slang words
+
+    Args:
+        text(str): raw text from student
+
+    Returns:
+        processed text or None
+    """
     respone = base_settings.client.models.generate_content(
         model=base_settings.model, contents=[base_settings.process_raw_text_promt, text]
     )
     return respone.text
 
 
-async def generate_json_from_text(text: str) -> dict[str, int | str]:
+async def generate_json_from_text(text: str) -> schemes.StudentsReview:
+    """
+    generate a StudentReview schema from processed text
+
+    Args:
+        text(str): processed text
+
+    Returns:
+        StudentReview schema
+
+    Raises:
+        AttributeError: if Gemini ai can't generate Json from processed text
+    """
     response = base_settings.client.models.generate_content(
         model=base_settings.model, contents=[]
     )
-    return json.loads(response.text or "")
+    if response.text is None:
+        raise AttributeError("Gemini response is None")
+
+    return schemes.StudentsReview.model_validate_json(response.text)
--- routers/callbacks.py
+++ routers/callbacks.py
@@ -1,13 +1,21 @@
 from aiogram.dispatcher.router import Router
 from aiogram.filters import CommandStart
 from aiogram.filters.command import Command
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import Message
 
 router = Router()
 
 
+class ReviewTeacherState(StatesGroup):
+    professors_name = State()
+    subject_name = State()
+    students_text = State()
+
+
 @router.message(CommandStart())
-async def start_command(meesage: Message):
+async def start_command(meesage: Message) -> None:
     """function to procces start command and send hello to user"""
 
     if meesage.from_user is None:
@@ -18,5 +26,36 @@ async def start_command(meesage: Message):
 
 
 @router.message(Command("review"))
-async def send_review(message: Message):
-    await message.answer("i got your review !")
+async def send_review(message: Message, state: FSMContext) -> None:
+    await state.set_state(ReviewTeacherState.professors_name)
+    await message.answer("Введите имя преподавателя: ")
+
+
+@router.message(ReviewTeacherState.professors_name)
+async def process_professors_name(message: Message, state: FSMContext) -> None:
+    await state.update_data(professors_name=message.text)
+    await state.set_state(ReviewTeacherState.subject_name)
+    await message.answer("Введите названия дицсиплины: ")
+
+
+@router.message(ReviewTeacherState.subject_name)
+async def process_subjects_name(message: Message, state: FSMContext) -> None:
+    await state.update_data(subject_name=message.text)
+    await state.set_state(ReviewTeacherState.students_text)
+    await message.answer("Введите свой отзыв о преподавателе")
+
+
+@router.message(ReviewTeacherState.students_text)
+async def process_student_text(message: Message, state: FSMContext) -> None:
+    user_data = await state.get_data()
+    professors_name = user_data.get("professors_name")
+    subjects_name = user_data.get("subject_name")
+    students_text = message.text
+    datetime = message.date
+
+    print(
+        f"professors name: {professors_name}\nsubjects name: {subjects_name}\nstudents text: {students_text}\ndatetime: {datetime}"
+    )
+
+    await message.answer("Благодарим за ваш отзыв !")
+    await state.clear()
